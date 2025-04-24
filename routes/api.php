08640faf980eb983ba3dcabe4c5581b16d18a63b
bao141004 <vuvanbao347@gmail.com>
2025-04-24 11:16:18 +0700

    done task

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\Api\ApiUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('post',ApiPostController::class);
-Route::apiResource('users',ApiUserController::class);
+Route::apiResource('user',ApiUserController::class);
 ?>
